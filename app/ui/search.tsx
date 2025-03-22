381ff4bdf90f5a1449d7f4b91dc2ab87696f344a
kaungyemyintmo <kaungyemo04@gmail.com>
2025-03-22 17:06:22 +0800

    feat: create invoice succeeded

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -14,16 +14,20 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
 
   // term is the user input
+  // debouncing delay the execution of a function until after a certain period of inactivity
+  // It helps improve performance by preventing excessive function calls, especially in cases like:
+  // Search input fields (API requests on each keystroke)
+  // Window resizing events
+  // Button click spam prevention
   const handleSearch = useDebouncedCallback((term) => {
     const params = new URLSearchParams(searchParams);
     console.log(term);
     params.set('page', '1');
-    if (term) { 
+    if (term) {
       params.set('query', term);
-    } else { 
+    } else {
       params.delete('query');
     }
-
     // pathname is the current path > /dashboard/invoices
     // params.toString() translates user input into a url friendly format
     // replace updates the url with the user input 
@@ -40,7 +44,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
-        onChange={(e) => {handleSearch(e.target.value);}}
+        onChange={(e) => { handleSearch(e.target.value); }}
         defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
